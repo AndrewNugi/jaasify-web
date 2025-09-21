86d83bcaea9b70209aa4877c8abac6121a548325
AndrewNugi <kiarienugi@gmail.com>
2025-09-21 10:28:18 +0300

    chore: Modifications for export

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+
+  // Optional: Change output directory name (default is 'out')
+  // distDir: 'dist',
 };
 
 export default nextConfig;
